0f5b6dc443cc6f94029d8061606b570bb7b8eda4
Mina Kamel <minallkamel@gmail.com>
2025-06-05 13:19:25 +0300

    add visittopics

--- main.go
+++ main.go
@@ -7,11 +7,12 @@ import (
 )
 
 var (
-	main_template        = template.Must(template.ParseFiles("templates/main.tmpl"))
-	mother_info_template = template.Must(template.ParseFiles("templates/motherinfo.tmpl"))
-	dad_info_template    = template.Must(template.ParseFiles("templates/dadinfo.tmpl"))
-	child_info_template  = template.Must(template.ParseFiles("templates/childinfo.tmpl"))
-	visit_info_template  = template.Must(template.ParseFiles("templates/visitinfo.tmpl"))
+	main_template         = template.Must(template.ParseFiles("templates/main.tmpl"))
+	mother_info_template  = template.Must(template.ParseFiles("templates/motherinfo.tmpl"))
+	dad_info_template     = template.Must(template.ParseFiles("templates/dadinfo.tmpl"))
+	child_info_template   = template.Must(template.ParseFiles("templates/childinfo.tmpl"))
+	visit_info_template   = template.Must(template.ParseFiles("templates/visitinfo.tmpl"))
+	visit_topics_template = template.Must(template.ParseFiles("templates/visittopics.tmpl"))
 )
 
 type IndexData struct {
@@ -24,24 +25,52 @@ type MotherInfo struct {
 	PhoneNumber string
 }
 
+func is_htmx(r *http.Request) bool {
+	return r.Header.Get("HX-Request") == "true"
+}
+
+func redirect_to_root(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+}
+
 func main() {
 	http.HandleFunc("/motherinfo", func(w http.ResponseWriter, r *http.Request) {
+		if !is_htmx(r) {
+			redirect_to_root(w, r)
+		}
 		if err := mother_info_template.Execute(w, nil); err != nil {
 			log.Fatal(err)
 		}
 	})
 	http.HandleFunc("/dadinfo", func(w http.ResponseWriter, r *http.Request) {
+		if !is_htmx(r) {
+			redirect_to_root(w, r)
+		}
 		if err := dad_info_template.Execute(w, nil); err != nil {
 			log.Fatal(err)
 		}
 	})
 	http.HandleFunc("/childinfo", func(w http.ResponseWriter, r *http.Request) {
+		if !is_htmx(r) {
+			redirect_to_root(w, r)
+		}
 		if err := child_info_template.Execute(w, nil); err != nil {
 			log.Fatal(err)
 		}
 	})
 	http.HandleFunc("/visitinfo", func(w http.ResponseWriter, r *http.Request) {
+		if !is_htmx(r) {
+			redirect_to_root(w, r)
+		}
 		if err := visit_info_template.Execute(w, nil); err != nil {
+			log.Fatal(err)
+		}
+	})
+	http.HandleFunc("/visittopics", func(w http.ResponseWriter, r *http.Request) {
+		if !is_htmx(r) {
+			redirect_to_root(w, r)
+		}
+		if err := visit_topics_template.Execute(w, nil); err != nil {
 			log.Fatal(err)
 		}
 	})
